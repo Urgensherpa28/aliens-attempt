c0f855a4094e2a393b5acebdd6dfd9f9f6576a69
github username <urgen.nyc@gmail.com>
2020-09-10 15:57:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+import Canvas from './components/Canvas'
+
+class App extends Component {
+  render() {
+    return (
+      <Canvas />
+      // <div className="App">
+      //   <h1>{this.props.message}</h1>
+      // </div>
+    );
+  }
+}
+
+// App.propTypes = {
+//   message: PropTypes.string.isRequired,
+// };
+
+export default App;
