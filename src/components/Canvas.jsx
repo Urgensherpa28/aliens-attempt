67aa449471989a49821c693d46097ae0c118f4d3
github username <urgen.nyc@gmail.com>
2020-09-10 16:34:33 -0400

    animated cannon

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import Sky from './Sky'
-import Ground from './Ground'
-import CannonPipe from './CannonPipe'
-import CannonBase from './CannonBase'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
 
+const Canvas = (props) => {
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+    </svg>
+  );
+};
 
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+};
 
-const Canvas = () => {
-    const viewBox = [window.innerWidth / -2, 100 -window.innerHeight, window.innerWidth, window.innerHeight]
-    return (
-        <svg
-        id='aliens-go-home-canvas'
-        preserveAspectRatio='xMaxYmax none'
-        viewBox={viewBox}
-        >
-            <Sky />
-            <Ground />
-            <CannonPipe rotation={45}/>
-            <CannonBase />
-        </svg>
-    )
-
-}
-
-export default Canvas 
+export default Canvas;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-const initialState = { 
-    message: `It's easy to integrate React and Redux`,
+import { MOVE_OBJECTS } from '../actions';
+import moveObjects from './moveObjects';
+
+const initialState = {
+  angle: 45,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS:
+      return moveObjects(state, action);
+    default:
+      return state;
+  }
 }
 
-function reducer(state = initialState) {
-    return state
-}
-
-export default reducer
+export default reducer;
